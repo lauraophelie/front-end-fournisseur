dc0e04cd2506f3e2bad259d99ebb5222ac902384
Laura Ophélie <lauraophelie1@gmail.com>
2023-11-26 09:24:31 +0100

    mail

--- front-fournisseur/src/components/MailContent.jsx
+++ front-fournisseur/src/components/MailContent.jsx
@@ -1,29 +1,50 @@
+/* eslint-disable react/prop-types */
 import { CardContent, Typography } from "@mui/material";
 import "../assets/mail.scss";
 
 function MailContent(props) {
     const { idMail } = props;
 
+    const mail = {
+        id: idMail,
+        entreprise: "DIMPEX",
+        objet: "Demande de proforma",
+        date: "2023-11-30",
+        heure: "15:00:00",
+        article: [
+            {
+                categorie: "Matériel informatique",
+                article: "Disque dur",
+                quantite: 50
+            },
+            {
+                categorie: "Matériel informatique",
+                article: "Clé USB",
+                quantite: 50
+            }
+        ]
+    };
+
     return (
         <>
             <div className="details-mails__infos">
                 <CardContent sx={{ width: 750, float: "left"}}>
                     <Typography sx={{ fontSize: 17 }} variant="h2" pl={3} pt={2}>
-                        Entreprise 
+                        {mail.entreprise}
                     </Typography>
                     
                     <Typography sx={{ fontSize: 15, fontWeight: "lighter", color: "grey"}} pl={3} pt={1}>
-                        Objet 
+                        {mail.objet}
                     </Typography>
                 </CardContent>
 
                 <CardContent sx={{ marginLeft: "30%"}}>
                     <Typography sx={{ fontSize: 17 }} variant="h2" pl={3} pt={2}>
-                        Date
+                        {mail.date}
                     </Typography>
                     
                     <Typography sx={{ fontSize: 15, fontWeight: "lighter", color: "grey"}} pl={3} pt={1}>
-                        Heure
+                        {mail.heure}
                     </Typography>
                 </CardContent>
             </div>
